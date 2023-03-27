247792dd95d0d4c1541c3a249cfa28619b4171e8
Safa Gayret <safa.gayret@tsoft.com.tr>
2023-03-27 10:47:26 +0300

    update: nav style

--- components/header.js
+++ components/header.js
@@ -6,10 +6,33 @@ export default function Header() {
         <Link href='/'>SG</Link>
       </div>
       <nav>
-        <Link href='/code'>Kod</Link>
-        <Link href='/movie'>Film</Link>
-        <Link href='/writing'>Yazı / Kitap</Link>
+        <Link href='/code'>
+          <a className='nav-item'>Kod</a>
+        </Link>
+        <Link href='/movie'>
+          <a className='nav-item'>Film</a>
+        </Link>
+        <Link href='/writing'>
+          <a className='nav-item'>Yazı / Kitap</a>
+        </Link>
       </nav>
+      <style jsx>{`
+        header {
+          padding: 1em 0;
+        }
+
+        .nav-item {
+          cursor: pointer;
+          border: 1px solid #fff;
+          border-radius: 0.5em;
+          padding: 0.5em 1em;
+        }
+
+        .nav-item:hover {
+          background-color: #fff;
+          color: #000;
+        }
+      `}</style>
     </header>
   )
 }
