cb402323c4cbb58b189bf05700a6393b01274e8f
Safa Gayret <safa.gayret@tsoft.com.tr>
2023-06-07 12:12:33 +0300

    fix: iframe attributes

--- pages/index.js
+++ pages/index.js
@@ -39,18 +39,18 @@ function Home() {
             height='315'
             src='https://www.youtube.com/embed/videoseries?list=PLY3mpMOY7G-fzfQMKbZnCwTsiFPA-yUgo'
             title='YouTube video player'
-            frameborder='0'
+            frameBorder='0'
             allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-            allowfullscreen
+            allowFullScreen
           ></iframe>
           <iframe
             width='560'
             height='315'
             src='https://www.youtube.com/embed/videoseries?list=PLY3mpMOY7G-cefEH8LVxzO95U7281FVI5'
             title='YouTube video player'
-            frameborder='0'
+            frameBorder='0'
             allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-            allowfullscreen
+            allowFullScreen
           ></iframe>
         </div>
 
