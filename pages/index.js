bd1e9fdea41ed43dae2fbf976f9d1e3b7b945445
Safa <safagayret@gmail.com>
2023-07-20 09:53:46 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -43,7 +43,7 @@ function Home() {
             allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
             allowFullScreen
           ></iframe>
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/DWm1RvYg0kI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/DWm1RvYg0kI" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
         </div>
 
         <ul>
