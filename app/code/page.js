5ee10084a0a2f028c88b6bccf0a2e5b092426b12
safa gayret <safagayret@gmail.com>
2024-09-21 12:41:49 +0300

    add: airtable conf, fetch

--- app/code/page.js
+++ app/code/page.js
@@ -1,11 +1,37 @@
 import TimeLineCard from '../components/TimeLineCard/timeLineCard'
-import timeLogs from './data.json'
+import Airtable from 'airtable'
 
-const getReversedTimeLogs = timeLogs.reverse()
+const code = []
+let result = []
+
+// Airtable data fetcher
+const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BASE);
+base('code').select({ view: "Grid view" }).eachPage(function page(records, fetchNextPage) {
+    records.forEach(function (record) {
+        code.push({
+            date: record.get('date'),
+            text: record.get('text')
+        })
+    });
+
+    const reduced = code.reduce((acc, curr) => {
+        if (!acc[curr.date]) {
+            acc[curr.date] = { date: curr.date, texts: [] };
+        }
+        acc[curr.date].texts.push(curr.text);
+        return acc;
+    }, {});
+
+    result = Object.values(reduced);
+
+    fetchNextPage();
+}, function done(err) {
+    if (err) { console.error(err); return; }
+})
 
 export default function Code() {
     return (<div className="bordered">
         <h1>Kod</h1>
-        {getReversedTimeLogs.map((time) => <TimeLineCard key={time.texts} texts={time.texts} date={time.date} link={time.link} />)}
+        {result.map((time) => <TimeLineCard key={time.texts} texts={time.texts} date={time.date} link={time.link} />)}
     </div>)
 }
