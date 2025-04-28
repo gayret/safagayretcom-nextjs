130d28f78918218a76826565755d7d9b48c16564
Safa Gayret <safagayret@gmail.com>
2025-04-28 10:47:50 +0300

    feat: invitation

---
+++ app/davet/page.js
@@ -0,0 +1,26 @@
+import Image from 'next/image'
+import invitationImage from '../assets/img/davetiye.jpg'
+
+export default function Davet() {
+  return (
+    <div className='bordered center'>
+      <h1>İrem & Safa&apos;nın Düğünü</h1>
+      <p>
+        <strong>Tarih:</strong> 18 Mayıs Pazar, saat 19:00
+      </p>
+
+      <br />
+
+      <p>
+        <strong>Konum:</strong>{' '}
+        <a href='https://maps.app.goo.gl/qdufjFsswXEji7si9'>
+          Korupark Events Düğün Salonu, Kahramanmaraş
+        </a>
+      </p>
+
+      <br />
+
+      <Image src={invitationImage} alt='Düğün Davetiyesi' width={300} />
+    </div>
+  )
+}
