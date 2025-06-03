3ddd640217c78616bb34c228447dd2ffce650bed
Safa Gayret <safagayret@gmail.com>
2025-06-03 09:28:36 +0300

    remove: davet, change: profile picture

--- app/components/Profile/Profile.js
+++ app/components/Profile/Profile.js
@@ -1,13 +1,13 @@
 import Image from 'next/image'
-import ProfilePicture from '../../assets/img/profile.webp'
+import ProfilePicture from '../../assets/img/ProfilePicture.webp'
 
 export default function Profile() {
   return (
     <section className='profile-box image-and-text'>
       <Image src={ProfilePicture} alt='profile picture' />
       <h1>Safa Gayret</h1>
-      <small>Yazılım Mühendisi</small>
-      <p>JavaScript, ReactJS, VueJS, NextJS</p>
+      <small>Yazılım Geliştirici</small>
+      <p>JavaScript, ReactJS, VueJS, NextJS, TypeScript</p>
     </section>
   )
 }
