38337b0855ac923686f072bf0e1b56299511b97f
Safa Gayret <safagayret@gmail.com>
2025-09-11 06:33:07 +0000

    feat: add book announcement

--- app/components/Profile/Profile.js
+++ app/components/Profile/Profile.js
@@ -1,13 +1,27 @@
 import Image from 'next/image'
 import ProfilePicture from '../../assets/img/ProfilePicture.webp'
+import BookPicture from './kitap.jpeg'
+import Link from 'next/link'
 
 export default function Profile() {
   return (
-    <section className='profile-box image-and-text'>
-      <Image src={ProfilePicture} alt='profile picture' />
-      <h1>Safa Gayret</h1>
-      <small>Kıdemli Yazılım Geliştirme Uzmanı</small>
-      <p>JavaScript, ReactJS, VueJS, TypeScript</p>
-    </section>
+    <>
+      <section className='profile-box image-and-text'>
+        <Image src={ProfilePicture} alt='profile picture' />
+        <h1>Safa Gayret</h1>
+        <small>Kıdemli Yazılım Geliştirme Uzmanı</small>
+        <p>JavaScript, ReactJS, VueJS, TypeScript</p>
+      </section>
+
+      <section>
+        <Link
+          className='book'
+          href='https://www.kitapyurdu.com/kitap/erken-bulunmus-bir-intihar-mektubu-/561410.html'
+        >
+          <Image src={BookPicture} alt='profile picture' width={100} />
+          <span>Yazdığım ilk roman</span>
+        </Link>
+      </section>
+    </>
   )
 }
